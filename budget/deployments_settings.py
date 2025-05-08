074a73477626f75c8cfe32c371912edaf946ba61
Mohammed Fasi <fasiulla103@gmail.com>
2025-05-08 21:20:23 +0530

    Updated deployment settings

--- budget/deployments_settings.py
+++ budget/deployments_settings.py
@@ -1,10 +1,10 @@
 import os
-import dj_database_url
 from .settings import *
 from .settings import BASE_DIR
+import dj_database_url
 
-ALLOWED_HOSTS=[os.environ.get['RENDER_EXTERNAL_HOST']]
-CSRF_TRUSTED_ORIGINS = ['https://'+os.environ.get['RENDER_EXTERNAL_HOST']]
+ALLOWED_HOSTS=[os.environ.get('RENDER_EXTERNAL_HOST')]
+CSRF_TRUSTED_ORIGINS = ['https://'+os.environ.get('RENDER_EXTERNAL_HOST')]
 
 DEBUG = False
 SECRET_KEY = os.environ.get['SECRET_KEY']
