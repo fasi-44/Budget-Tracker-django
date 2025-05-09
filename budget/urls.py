cd44c64418fbd245d9888b2e63f15665f53802a6
Mohammed Fasi <fasiulla103@gmail.com>
2025-05-09 18:17:55 +0530

    changes

--- budget/urls.py
+++ budget/urls.py
@@ -1,5 +1,14 @@
-from django.urls import path, include
+from django.urls import path, include, reverse
+from django.http import HttpResponse
+from django.shortcuts import redirect
+
+def root_view(request):
+    """
+    Redirects the user to the login view.
+    """
+    return redirect(reverse('login'))  # 'login' is the name given in api/urls.py
 
 urlpatterns = [
-    path("", include("api.urls")),
+    path("", root_view),
+    path("api/", include("api.urls")),
 ]
