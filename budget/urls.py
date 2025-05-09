3d677df442e8d2e141889f7cb79a83e2a8636c26
Mohammed Fasi <fasiulla103@gmail.com>
2025-05-09 17:59:25 +0530

    redirect to login page

--- budget/urls.py
+++ budget/urls.py
@@ -1,15 +1,6 @@
 from django.urls import path, include
 from django.http import HttpResponse
 
-def root_view(request):
-    """
-    This view will handle requests to the root URL ("/").
-    You can customize this to return a simple message,
-    a welcome page, or redirect to another part of your application.
-    """
-    return HttpResponse("Welcome to your Budget Tracker API!")  # Simplest response
-
 urlpatterns = [
-    path("", root_view),  # Add this line for the root URL
     path("api/", include("api.urls")),
 ]
